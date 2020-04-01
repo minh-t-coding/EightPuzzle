3b8e1e3be2ecdb4b19720b84769ea5fc82b1ad64
Minh Nguyen <melnguyen115@gmail.com>
2020-04-01 14:27:08 -0700

    added input file??? also some stuff to read the input file

--- src/hw03_minhnguyen.java
+++ src/hw03_minhnguyen.java
@@ -1,4 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class hw03_minhnguyen {
+	public static void readInput(String filename) {
+		try {
+			Scanner in = new Scanner(new FileReader(filename));
+			
+			while(in.hasNext()) {
+				System.out.println(in.next());
+			}
+			
+		} catch(FileNotFoundException e) {
+			System.out.println("The file " + filename + " was not found.");
+		}
+	}
+	
 	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.out.println("ERROR: Missing argument!");
@@ -6,5 +22,6 @@ public class hw03_minhnguyen {
 			System.exit(1);
 		}
 		
+		readInput(args[0]);
 	}
 }
