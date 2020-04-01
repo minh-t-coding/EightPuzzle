b41584b80a68203d43001d1da499e576457044bb
Minh Nguyen <melnguyen115@gmail.com>
2020-04-01 13:51:12 -0700

    added basic files

---
+++ src/Puzzle.java
@@ -0,0 +1,10 @@
+public class Puzzle {
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("ERROR: Missing argument!");
+			System.out.println("Usage: java Puzzle \"input.txt\"");
+			System.exit(1);
+		}
+		
+	}
+}
